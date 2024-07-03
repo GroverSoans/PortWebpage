552df6014787a53bf0caa971c129b7d2012b876e
GroverSoans <groversoans@Grovers-Air.zyxel.com>
2024-07-02 20:04:15 -0500

    Infinite Logo Scroll Bar

---
+++ src/components/LogoSlider.jsx
@@ -0,0 +1,62 @@
+import './css/scroll.css'
+import { RiJavaFill } from "react-icons/ri";
+import { SiMicrosoftazure, SiFirebase, SiTailwindcss, SiDjango, SiReact, SiNodedotjs,
+    SiMongodb, SiBootstrap, SiGit, SiGithub, SiHtml5, SiCss3, SiJavascript, SiPython,
+    SiCplusplus } from "react-icons/si";
+
+
+const LogoSlider = () => {
+
+
+
+const items = [
+  { word:"Java", icon: <RiJavaFill/> },
+  { word:"Microsoft Azure", icon: <SiMicrosoftazure/> },
+  { word:"Google Firebase", icon: <SiFirebase/> },
+  { word:"Tailwind CSS", icon: <SiTailwindcss/> },
+  { word:"Django", icon: <SiDjango/> },
+  { word:"React", icon: <SiReact/> },
+  { word:"NodeJs", icon: <SiNodedotjs/> },
+  { word:"Mongodb", icon: <SiMongodb/> },
+  { word:"Bootstrap", icon: <SiBootstrap/> },
+  { word:"Git", icon: <SiGit/> },
+  { word:"HTML5", icon: <SiHtml5/> },
+  { word:"CSS", icon: <SiCss3/> },
+  { word:"Github", icon: <SiGithub/> },
+  { word:"Javascript", icon: <SiJavascript/> },
+  { word:"Python", icon: <SiPython/> },
+  { word:"C++", icon: <SiCplusplus/> },
+];
+
+return (
+  <div className="bottom-0 overflow-hidden w-full h-20 bg-transparent relative ">
+    <div className="absolute top-0 left-0 w-full h-full flex animate-scroll">
+      <div className="flex items-center">
+        {items.map((item, index) => (
+            <div
+              key={index}
+              className="w-auto h-10 bg-black p-3 m-3 flex items-center justify-center text-white text-nowrap rounded-md"
+            >
+              <div className="mr-1">{item.icon}</div>
+              <span>{item.word}</span>           
+            </div>
+          ))}
+          {items.map((item, index) => (
+            <div
+              key={`${index}-duplicate`}
+              className="w-auto h-10 bg-black p-3 m-3 flex items-center justify-center text-white text-nowrap"
+            >
+              <div className="w-6 h-6 mb-1">{item.icon}</div>
+              <span>{item.word}</span>
+            </div>
+          ))}
+      </div>
+    </div>
+    <div className="absolute inset-0 bg-gradient-to-r from-white via-transparent to-white pointer-events-none"></div>
+  </div>
+  )
+};
+
+
+export default LogoSlider
+
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
+import LogoSlider from "../components/LogoSlider"
+
 const About = () => {
   return (
-    <div id='about' className='bg-white h-screen w-screen'>
+    <div id='about' className='bg-white h-screen w-screen flex flex-col justify-between'>
       <div>
         {/* Transition elements in an out from the left and right that has information reguarding my skills */}
       </div>
-      <h1 id='about' className='flex align-middle justify-center items-center font-extrabold text-black opacity-30 text-[470px] pt-20'>About<span className='text-[#CFFF04]'>.</span></h1>
-      <div>
-        {/* Add a sliding banner that showcases all the technoligies that i am familiar with */}
+      <h1 id='about' className='absolute justify-center items-center font-extrabold text-black opacity-30 text-[470px] pt-20'>About<span className='text-[#CFFF04]'>.</span></h1>
+      <div className="relative">
+        <LogoSlider/>
       </div>
     </div>
 
