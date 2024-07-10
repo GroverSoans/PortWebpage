79a73bdee1d8b83abd167d060b8376468d474e57
GroverSoans <groversoans@Grovers-MacBook-Air.local>
2024-07-10 18:34:09 -0500

    sliderContent Data file added.

---
+++ src/constants/sliderContent.js
@@ -0,0 +1,34 @@
+
+const sliderContent = [
+    {
+        title:"Neo Forge Towers",
+        image:"./src/assets/img1.jpg"
+    },
+    {
+        title:"Arcadian Complex",
+        image:"./src/assets/img2.jpg"
+    },
+    {
+        title:"Shadowline Spire",
+        image:"./src/assets/img3.jpg"
+    },
+    {
+        title:"Echo Nexus Habitat",
+        image:"./src/assets/img4.jpg"
+    },
+    {
+        title:"Cascade Enclave",
+        image:"./src/assets/img5.jpg"
+    },
+    {
+        title:"Prism Sector",
+        image:"./src/assets/img6.jpg"
+    },
+    {
+        title:"Iron Eden Colony",
+        image:"./src/assets/img7.jpg"
+    },
+    
+];
+
+export default sliderContent;
